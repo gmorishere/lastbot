1a063a6f37597973be2ab4cfe67a56a1bfb0c4e8
ryangmor <aryan.shahin@icloud.com>
2016-01-07 14:26:30 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (shieldbot)",-- Put everything you like :)
+		"^[!/]([Sh]ieldbot)"-- Put everything you like :)
     }, 
   run = run
 }
