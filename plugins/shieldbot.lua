0d38efebbfe6db941fdfcbbfd0c59795d83feed9
ryangmor <aryan.shahin@icloud.com>
2016-01-13 01:15:58 +0330

    Create shieldbot.lua

---
+++ plugins/shieldbot.lua
@@ -0,0 +1,34 @@
+do
+
+function run(msg, matches)
+  return " Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ this bot is made by :@Gmorr 〰〰〰〰〰〰〰〰 🚩 Our admins are : 🔰 @Dawsh_Sepehr 🔰 @mehrdadparsa_admin_devils 🔰 @RyanGmmor 〰〰〰〰〰〰〰〰 ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command : !feedback (your ideas and messages) "
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^shieldbot$",
+    "^/shieldbot$",
+    "^!shieldbot$",
+    "^/Shieldbot$",
+    "^!Shieldbot$",
+  },
+  run = run
+}
+end
+
+functn run(msg, matches)
+  return " 
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^/sudoers$",
+    "^!sudoers$",
+    "^sudoers$",
+  },
+  run = run
+}
+end
