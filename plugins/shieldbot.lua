5f0043c985732086269551203e79bec470ea625a
ryangmor <aryan.shahin@icloud.com>
2016-01-13 01:21:01 +0330

    shield

--- plugins/shieldbot.lua
+++ plugins/shieldbot.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return " Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ this bot is made by :@Gmorr 〰〰〰〰〰〰〰〰 🚩 Our admins are : 🔰 @Dawsh_Sepehr 🔰 @mehrdadparsa_admin_devils 🔰 @RyanGmmor 〰〰〰〰〰〰〰〰 ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command : !feedback (your ideas and messages) "
+  return " Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ \nthis bot is made by :@Gmorr \n〰〰〰〰〰〰〰〰\n 🚩 Our admins are : \n🔰 @Dawsh_Sepehr \n🔰 @mehrdadparsa_admin_devils \n🔰 @RyanGmor \n〰〰〰〰〰〰〰〰 \n♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :\n !feedback (your ideas and messages) "
   end
 return {
   description = "shows sudoers", 
@@ -12,22 +12,6 @@ return {
     "^!shieldbot$",
     "^/Shieldbot$",
     "^!Shieldbot$",
-  },
-  run = run
-}
-end
-
-functn run(msg, matches)
-  return " 
-  end
-return {
-  description = "shows sudoers", 
-  usage = "!sudoers : return sudousers",
-  patterns = {
-    "^SUDOERS$",
-    "^/sudoers$",
-    "^!sudoers$",
-    "^sudoers$",
   },
   run = run
 }
