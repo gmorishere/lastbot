68a1e1a48c1953d7196c091997ce1ba9ece38dc3
ryangmor <aryan.shahin@icloud.com>
2016-01-13 19:04:33 +0330

    Update boobs.lua

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -1,9 +1,9 @@
 do
 
 -- Recursive function
-local function getRandomButts(attempt),getRandomButts(attempt),getRandomButts(attempt),getRandomButts(attempt)
+local function getRandomButts(attempt)
   attempt = attempt or 0
-  attempt = attempt + 10
+  attempt = attempt + 1
 
   local res,status = http.request("http://api.obutts.ru/noise/1")
 
@@ -13,15 +13,15 @@ local function getRandomButts(attempt),getRandomButts(attempt),getRandomButts(at
   -- The OpenBoobs API sometimes returns an empty array
   if not data and attempt <= 3 then
     print('Cannot get that butts, trying another one...')
-    return getRandomButts(attempt),getRandomButts(attempt),getRandomButts(attempt),getRandomButts(attempt)
+    return getRandomButts(attempt)
   end
 
   return 'http://media.obutts.ru/' .. data.preview
 end
 
-local function getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt)
+local function getRandomBoobs(attempt)
   attempt = attempt or 0
-  attempt = attempt + 10
+  attempt = attempt + 1
 
   local res,status = http.request("http://api.oboobs.ru/noise/1")
 
@@ -31,7 +31,7 @@ local function getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(at
   -- The OpenBoobs API sometimes returns an empty array
   if not data and attempt < 10 then 
     print('Cannot get that boobs, trying another one...')
-    return getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt),getRandomBoobs(attempt)
+    return getRandomBoobs(attempt)
   end
 
   return 'http://media.oboobs.ru/' .. data.preview
@@ -41,11 +41,11 @@ local function run(msg, matches)
   local url = nil
   
   if matches[1] == "boobs" then
-    url = getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs(),getRandomBoobs()
+    url = getRandomBoobs()
   end
 
   if matches[1] == "butts" then
-    url = getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts(),getRandomButts()
+    url = getRandomButts()
   end
 
   if url ~= nil then
