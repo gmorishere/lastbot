06485825d2549516e6d29c8e5597711cf258c6db
ryangmor <aryan.shahin@icloud.com>
2016-01-13 15:33:25 +0330

    Update shieldbot.lua

--- plugins/shieldbot.lua
+++ plugins/shieldbot.lua
@@ -1,11 +1,15 @@
-do
-
-function run(msg, matches)
-  return " Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ \nthis bot is made by :@Gmorr \n〰〰〰〰〰〰〰〰\n 🚩 Our admins are : \n🔰 @Dawsh_Sepehr \n🔰 @mehrdadparsa_admin_devils \n🔰 @RyanGmor \n〰〰〰〰〰〰〰〰 \n♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :\n !feedback (your ideas and messages) "
+local function run(msg, matches)
+	if not is_momod(msg) then 
+		return "Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ \nthis bot is made by :@Gmorr \n〰〰〰〰〰〰〰〰\n 🚩 Our admins are : \n🔰 @Dawsh_Sepehr \n🔰 @mehrdadparsa_admin_devils \n🔰 @RyanGmor \n〰〰〰〰〰〰〰〰 \n♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :\n !feedback (your ideas and messages)"
+	end
+	if matches[1] then
+		return " Shield bot 3.2 Hello my Good friends 😀🖐🏻 ‼️ \nthis bot is made by :@Gmorr \n〰〰〰〰〰〰〰〰\n 🚩 Our admins are : \n🔰 @Dawsh_Sepehr \n🔰 @mehrdadparsa_admin_devils \n🔰 @RyanGmor \n〰〰〰〰〰〰〰〰 \n♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :\n !feedback (your ideas and messages) "
   end
-return {
-  description = "shows sudoers", 
-  usage = "!sudoers : return sudousers",
+  return
+end
+ return {
+  description = "",
+  usage = {""},
   patterns = {
     "^shieldbot$",
     "^/shieldbot$",
@@ -15,4 +19,3 @@ return {
   },
   run = run
 }
-end
